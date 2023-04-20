e207e7cb4c74c1700a689be3ddc1b17535004438
小翼 <leo@tiiko.cn>
2023-04-21 01:33:05 +0400

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,15 @@
-steamcmd +force_install_dir /serverfiles +login anonymous +app_update 380870 validate +quit
-cp /serverfiles/ProjectZomboid64.json.bak /serverfiles/ProjectZomboid64.json
-exec /serverfiles/start-server.sh 
+echo "------------ PREPARE RUNTIME ENV ------------"
+export INSTDIR="/serverfiles"
+export PATH="${INSTDIR}/jre64/bin:$PATH"
+export JSIG="libjsig.so"
+export LD_LIBRARY_PATH="${INSTDIR}/linux64:${INSTDIR}/natives:${INSTDIR}:${INSTDIR}/jre64/lib/amd64:${LD_LIBRARY_PATH}"
+LD_PRELOAD="${LD_PRELOAD}:${JSIG}"
+echo "------------ RUNTIME ENV SETED ------------"
+
+echo "------------ CHECKING FOR UPDATES ------------"
+steamcmd +force_install_dir "${INSTDIR}" /serverfiles +login anonymous +app_update 380870 +quit
+echo "------------ UPDATES CHECKED ------------"
+
+echo "------------ STARTING SERVER ------------"
+awk '/-Dzomboid.znetlog=1/ { print; print "\"-Duser.language=cn\","; print "\"-Duser.region=CN\","; next }1' ProjectZomboid64.json > temp.json && mv temp.json ProjectZomboid64.json
+exec ./ProjectZomboid64 "$STARTUP_ARGS"
